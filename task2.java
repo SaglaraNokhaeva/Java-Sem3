2158d81aff64e86257048a0b551f80911347e8eb
Saglara <nohaevass@yandex.ru>
2023-01-28 18:54:22 +0300

    Update task2.java

--- task2.java
+++ task2.java
@@ -9,15 +9,15 @@ public class task2 {
     public static void main(String[] args) {
 
         ArrayList<String> list = new ArrayList<>();
-        list.add(0, "Земля");
-        list.add(0, "Марс");
-        list.add(0, "Юпитер");
-        list.add(0, "Юпитер");
-        list.add(0, "Венера");
-        list.add(0, "Плутон");
-        list.add(0, "Сатурн");
-        list.add(0, "Земля");
-        list.add(0, "Земля");
+        list.add( "Земля");
+        list.add( "Марс");
+        list.add("Юпитер");
+        list.add("Юпитер");
+        list.add("Венера");
+        list.add("Плутон");
+        list.add("Сатурн");
+        list.add("Земля");
+        list.add("Земля");
         System.out.println(list);
 
         list.sort(Comparator.naturalOrder());
